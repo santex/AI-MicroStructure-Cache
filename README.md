daa6672945dc116d8f87d76d9873843fa1cf89c9
hagen <hagen@sante12.(none)>
2013-01-15 16:04:48 +0100

    prerequests

--- README.md
+++ README.md
@@ -2,3 +2,53 @@ AI-MicroStructure-Cache
 =======================
 
 this is part of active-memory.  the cache will be containing micro structure parts in http user session context
+
+
+=head1 NAME
+  AI::MicroStructure
+=head1 DESCRIPTION
+  Creates Concepts for words  
+=head1 SYNOPSIS    
+  
+  ~$ micro new world
+  
+  ~$ micro themes
+  
+  ~$ micro any 2
+  
+  ~$ micro drop world
+  
+  ~$ micro
+  
+=head1 AUTHOR
+
+  Hagen Geissler <santex@cpan.org>
+
+=head1 COPYRIGHT AND LICENCE
+
+  Hagen Geissler <santex@cpan.org>
+
+=head1 SUPPORT AND DOCUMENTATION
+
+  ☞ [sample using concepts](http://quantup.com)
+
+  ☞ [PDF info on my works](https://github.com/santex)
+
+  
+=head1 SEE ALSO
+
+  AI-MicroStructure
+  AI-MicroStructure-Cache
+  AI-MicroStructure-Deamon
+  AI-MicroStructure-Relations
+  AI-MicroStructure-Concept
+  AI-MicroStructure-Data
+  AI-MicroStructure-Driver
+  AI-MicroStructure-Plugin-Pdf
+  AI-MicroStructure-Plugin-Twitter
+  AI-MicroStructure-Plugin-Wiki
+
+  
+__END__
+__DATA__
+
--- lib/AI/MicroStructure/Cache.pm
+++ lib/AI/MicroStructure/Cache.pm
@@ -130,4 +130,54 @@ sub includes_name  { exists $_[0]->{ $_[1] } }
 
 
 1;
-# ABSTRACT: turns baubles into trinkets
+# ABSTRACT: this is part of AI-MicroStructure the cache will be containing micro structure parts in http user session context
+
+
+=head1 NAME
+  AI::MicroStructure::Cache
+=head1 DESCRIPTION
+  Creates Concepts for words  
+=head1 SYNOPSIS    
+  
+  ~$ micro new world
+  
+  ~$ micro themes
+  
+  ~$ micro any 2
+  
+  ~$ micro drop world
+  
+  ~$ micro
+  
+=head1 AUTHOR
+
+  Hagen Geissler <santex@cpan.org>
+
+=head1 COPYRIGHT AND LICENCE
+
+  Hagen Geissler <santex@cpan.org>
+
+=head1 SUPPORT AND DOCUMENTATION
+
+  ☞ [sample using concepts](http://quantup.com)
+
+  ☞ [PDF info on my works](https://github.com/santex)
+
+  
+=head1 SEE ALSO
+
+  AI-MicroStructure
+  AI-MicroStructure-Cache
+  AI-MicroStructure-Deamon
+  AI-MicroStructure-Relations
+  AI-MicroStructure-Concept
+  AI-MicroStructure-Data
+  AI-MicroStructure-Driver
+  AI-MicroStructure-Plugin-Pdf
+  AI-MicroStructure-Plugin-Twitter
+  AI-MicroStructure-Plugin-Wiki
+
+  
+__END__
+__DATA__
+
